7f336bc4bf473d6953e302cb6ac9e2a6c5cc65e4
aivencs <aivenc@foxmail.com>
2022-03-18 22:14:28 +0800

    dev(kits):coding kits

---
+++ pkg/kits/default.go
@@ -0,0 +1,33 @@
+package kits
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"sort"
+)
+
+// 生成消息摘要
+func CreateDigest(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// 元素是否在数组中
+func IsContainString(target string, raw []string) bool {
+	sort.Strings(raw)
+	index := sort.SearchStrings(raw, target)
+	if index < len(raw) && raw[index] == target {
+		return true
+	}
+	return false
+}
+
+func IsContainInt(target int, raw []int) bool {
+	sort.Ints(raw)
+	index := sort.SearchInts(raw, target)
+	if index < len(raw) && raw[index] == target {
+		return true
+	}
+	return false
+}
