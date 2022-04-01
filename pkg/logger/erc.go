fa2e32bc0ce43a6e8a30468ce82d07d7ae64922e
aivencs <aivenc@foxmail.com>
2022-04-01 13:17:06 +0800

    dev(logger):调整文案

--- pkg/logger/erc.go
+++ pkg/logger/erc.go
@@ -3,7 +3,7 @@ package logger
 import "unicode/utf8"
 
 var erc map[string]Erc
-var defaultName = "success"
+var defaultName = "SUCCESS"
 
 type Erc struct {
 	Name  string
@@ -14,21 +14,21 @@ type Erc struct {
 
 func InitErc() {
 	erc = map[string]Erc{
-		"success":      {Label: "操作成功", Code: 10000, Level: "info"},
-		"check":        {Label: "操作失败", Code: 10001, Level: "check"},
-		"overload":     {Label: "超限", Code: 10002, Level: "error"},
-		"timeout":      {Label: "超时", Code: 10003, Level: "error"},
-		"supp":         {Label: "补充数据", Code: 10004, Level: "warn"},
-		"abnormal":     {Label: "非常规状态码", Code: 10005, Level: "error"},
-		"ede":          {Label: "编码/解码失败", Code: 10006, Level: "error"},
-		"rpe":          {Label: "运行时参数错误", Code: 10007, Level: "error"},
-		"pve":          {Label: "参数未通过校验", Code: 10008, Level: "error"},
-		"dve":          {Label: "数据结果未通过校验", Code: 10009, Level: "error"},
-		"rwa":          {Label: "运行时发生异常", Code: 10010, Level: "warn"},
-		"rpw":          {Label: "运行时参数错误", Code: 10011, Level: "warn"},
-		"timeout-call": {Label: "调用超时", Code: 20001, Level: "check"},
-		"error-call":   {Label: "调用错误", Code: 20002, Level: "error"},
-		"interrupt":    {Label: "组件中断", Code: 30001, Level: "fatal"},
+		"SUCCESS":      {Label: "操作成功", Code: 10000, Level: "info"},
+		"CHECK":        {Label: "请检查", Code: 10001, Level: "check"},
+		"OVERLOAD":     {Label: "超限", Code: 10002, Level: "error"},
+		"TIMEOUT":      {Label: "超时", Code: 10003, Level: "error"},
+		"SUPP":         {Label: "补充数据", Code: 10004, Level: "warn"},
+		"ABNORMAL":     {Label: "非常规状态码", Code: 10005, Level: "error"},
+		"EDE":          {Label: "编码/解码失败", Code: 10006, Level: "error"},
+		"RPE":          {Label: "运行时参数错误", Code: 10007, Level: "error"},
+		"PVE":          {Label: "参数未通过校验", Code: 10008, Level: "error"},
+		"DVE":          {Label: "数据结果未通过校验", Code: 10009, Level: "error"},
+		"RWA":          {Label: "运行时发生异常", Code: 10010, Level: "warn"},
+		"RPW":          {Label: "运行时参数错误", Code: 10011, Level: "warn"},
+		"CALL-TIMEOUT": {Label: "调用超时", Code: 20001, Level: "check"},
+		"CALL_ERROR":   {Label: "调用错误", Code: 20002, Level: "error"},
+		"INTERRUPT":    {Label: "组件中断", Code: 30001, Level: "fatal"},
 	}
 }
 
